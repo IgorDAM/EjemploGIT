6dc0dde94d1c04a6f4bb48945b610d4d752423e2
IgorDAM <151687081+IgorDAM@users.noreply.github.com>
2023-12-05 21:12:29 +0100

    Update domingo2.java

--- PruebaGit/src/main/java/modelo/MiClase.java
+++ PruebaGit/src/main/java/modelo/MiClase.java
@@ -6,6 +6,7 @@ public class MiClase implements Miinterfaz{
 
 	@Override
 	public int metododevuelveentero() {
+		//No me dio tiempo a implementar este método
 		// TODO Auto-generated method stub
 		return 0;
 	}
